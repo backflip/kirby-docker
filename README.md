3c7ccc690c1e9fa4677ebd9e60624dcfce79eaa4
Thomas Jaggi <thomas@responsive.ch>
2026-06-15 16:51:47 +0200

    feat: consolidate into single volume

--- README.md
+++ README.md
@@ -11,5 +11,5 @@ docker build . -t kirby
 ## Run
 
 ```sh
-docker run -p 8080:8080 -v $(pwd)/content:/var/www/html/content -v $(pwd)/site:/var/www/html/site -t kirby
+docker run -p 8080:8080 -v $(pwd)/data:/var/www/html/data -t kirby
 ```
--- index.php
+++ index.php
@@ -2,4 +2,9 @@
 
 require 'kirby/bootstrap.php';
 
-echo (new Kirby)->render();
+echo (new Kirby([
+    'roots' => [
+        'content'  => __DIR__ . '/data/content',
+        'accounts' => __DIR__ . '/data/accounts',
+    ]
+]))->render();
