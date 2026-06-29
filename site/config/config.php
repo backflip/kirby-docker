ef5375052797c3ef3b9b9698ee46e5b75c45a96c
Thomas Jaggi <thomas@responsive.ch>
2026-06-29 11:43:20 +0200

    feat: enable debug mode for admin users

--- site/config/config.php
+++ site/config/config.php
@@ -1,7 +1,13 @@
 <?php
 
-// Kirby is unable to derive the URL when using nginx, port is incorrect, e.g.
-// So we pass it as global variable instead
 return [
-    'url' => $_SERVER["APP_URL"]
+    // Kirby is unable to derive the URL when using nginx, port is incorrect, e.g.
+    // So we pass it as global variable instead
+    'url' => $_SERVER["APP_URL"],
+    // Enable debug mode for admin users
+    'ready' => function ($kirby) {
+        return [
+            'debug' => kirby()->user() && kirby()->user()->role()->isAdmin()
+        ];
+    }
 ];
