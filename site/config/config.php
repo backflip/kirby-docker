e4a20b3908ed29848d247069f33a9433a78caf9d
Thomas Jaggi <thomas@responsive.ch>
2026-06-29 16:06:53 +0200

    feat: handle env outside of nginx

--- site/config/config.php
+++ site/config/config.php
@@ -3,7 +3,7 @@
 return [
     // Kirby is unable to derive the URL when using nginx, port is incorrect, e.g.
     // So we pass it as global variable instead
-    'url' => $_SERVER["APP_URL"],
+    'url' => $_SERVER['APP_URL'] ?? getenv('APP_URL'),
     // Enable debug mode for admin users
     'ready' => function ($kirby) {
         return [
